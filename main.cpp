54fb744dd78603f01188b3e7090a9155a62e6930
Anton K <chuvibaka23@gmail.com>
2026-09-16 17:37:35 +0300

    Add calculator project with CMake build

---
+++ main.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+
+class Calculator
+{
+public:
+	Calculator()
+	{
+	}
+
+	Calculator(float startValue)
+	{
+		memory = startValue;
+	}
+
+	float calculate(char op, float value2)
+	{
+		if (op == '+')
+		{
+			memory = memory + value2;
+		}
+		if (op == '-')
+		{
+			memory = memory - value2;
+		}
+		if (op == '*')
+		{
+			memory = memory * value2;
+		}
+		if (op == '/')
+		{
+			if (!value2)
+			{
+				std::cout << "You cannot divide by 0\n";
+				return memory;
+			}
+			memory = memory / value2;
+		}
+		return memory;
+	}
+
+	void setStartValue(float start)
+	{
+		memory = start;
+	}
+
+	float calculate(float value1, float value2, char op)
+	{
+		float result = 0;
+		if (op == '+')
+		{
+			result = value1 + value2;
+		}
+		if (op == '-')
+		{
+			result = value1 - value2;
+		}
+		if (op == '*')
+		{
+			result = value1 * value2;
+		}
+		if (op == '/')
+		{
+			if (!value2)
+			{
+				std::cout << "You cannot divide by 0\n";
+				return 0;
+			}
+			result = value1 / value2;
+		}
+		return result;
+	}
+
+private:
+	float memory = 0;
+};
+
+int main()
+{
+	Calculator calc0;
+
+	std::cout << "Default constructor:\n";
+	std::cout << "0 + 5 = " << calc0.calculate('+', 5) << "\n";
+	std::cout << "5 * 3 = " << calc0.calculate('*', 3) << "\n";
+
+	Calculator calc1 = 10;
+
+	std::cout << "Constructor with start value 10:\n";
+	std::cout << "10 - 10 = " << calc1.calculate('-', 10) << "\n";
+	std::cout << "0 + 7 = " << calc1.calculate('+', 7) << "\n";
+
+	Calculator calc2(100);
+
+	std::cout << "Constructor with start value 100:\n";
+	std::cout << "100 / 4 = " << calc2.calculate('/', 4) << "\n";
+
+	Calculator calc3;
+
+	float startValue;
+	std::cout << "Enter start value: ";
+	std::cin >> startValue;
+	calc3.setStartValue(startValue);
+
+	std::cout << "Memory: " << startValue << "\n";
+	std::cout << "Enter operations, or q to quit:\n";
+
+	char operation;
+	float value;
+	while (std::cin >> operation)
+	{
+		if (operation == 'q')
+		{
+			break;
+		}
+
+		std::cin >> value;
+		std::cout << "= " << calc3.calculate(operation, value) << "\n";
+	}
+
+	return 0;
+}
